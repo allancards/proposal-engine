041668585503fd0a0b8a23aae274cb85010c244e
allancards <allancardspositivo@gmail.com>
2026-08-20 11:03:57 -0300

    fix: removendo textos fixos

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,6 @@ export default function NewProposalLayout({ children }: NewProposalLayoutProps) 
     <div className="new-proposal-container">
       {/* Você pode adicionar elementos fixos aqui, como uma barra lateral ou título */}
       <header className="mb-4">
-        <h1 className="text-2xl font-bold">Criar Nova Proposta</h1>
       </header>
       
       {/* O Next.js injeta a página (page.tsx) dentro do "children" */}
